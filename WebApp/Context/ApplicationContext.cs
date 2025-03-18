56af8e45dcb324581cfef909d74db37d66b81d79
gluisjulian <gluisjulian@gmail.com>
2025-03-17 21:21:56 -0300

    Criação

---
+++ WebApp/Context/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) :base(options)
+        {
+            
+        }
+
+        
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Pessoa>().HasKey(p => p.Id);
+        }
+    }
+}
